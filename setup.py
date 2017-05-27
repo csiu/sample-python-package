1a82506afebdd37d9f0db1b9ffdfcdc3b2ed265d
csiu <celia.sy.siu@gmail.com>
2017-05-27 01:53:07 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='mypackage',
+      version='0.1',
+      author='Celia Siu',
+      author_email='fake-email@example.com',
+      url='http://github.com/csiu/sample-python-package',
+      description='Sample python package',
+      long_description='',
+      #install_requires=['pybind11>=2.0', 'numpy'],
+      #cmdclass={'build_ext': BuildExt},
+      #test_suite="tests",
+      license='MIT',
+      zip_safe=False)
